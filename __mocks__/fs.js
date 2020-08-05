2a6c7a1968702025e930340141c920f3eb6e9022
EnochQin <qyqdream@gmail.com>
2020-08-05 22:14:19 +0800

    test: db.write

--- __mocks__/fs.js
+++ __mocks__/fs.js
@@ -2,19 +2,37 @@ const fs = jest.genMockFromModule('fs');
 const _fs = jest.requireActual('fs');
 Object.assign(fs, _fs);
 
-const mocks = {};
-
-fs.setMock = (path, error, data) => {
-  mocks[path] = [error, data];
+let readMocks = {};
+fs.setReadFileMock = (path, error, data) => {
+  readMocks[path] = [error, data];
 };
 
 fs.readFile = (path, options, callback) => {
   if (!callback) callback = options;
-  if (path in mocks) {
-    callback(...mocks[path]);
+  if (path in readMocks) {
+    callback(...readMocks[path]);
   } else {
     _fs.readFile(path, options, callback);
   }
+};
+
+let writeMocks = {};
+fs.setWriteFileMock = (path, fn) => {
+  writeMocks[path] = fn;
+};
+
+fs.writeFile = (path, data, options, callback) => {
+  if (!callback) callback = options;
+  if (path in writeMocks) {
+    writeMocks[path](path, data, options, callback);
+  } else {
+    _fs.writeFile(path, data, options, callback);
+  }
+};
+
+fs.clearMocks = () => {
+  readMocks = {};
+  writeMocks = {};
 };
 
 module.exports = fs;
--- __tests__/db.spec.js
+++ __tests__/db.spec.js
@@ -3,10 +3,27 @@ const fs = require('fs');
 jest.mock('fs');
 
 describe('db', () => {
+  afterEach(() => {
+    fs.clearMocks();
+  });
   it('can read', async () => {
     const data = [{ title: 'test task', done: false }];
-    fs.setMock('/testPath', null, JSON.stringify(data));
-    const list = await db.read('/testPath');
+    fs.setReadFileMock('/testReadPath', null, JSON.stringify(data));
+    const list = await db.read('/testReadPath');
     expect(list).toStrictEqual(data);
+  });
+
+  it('can write', async () => {
+    let fakeFile;
+    fs.setWriteFileMock('/testWritePath', (path, data, callback) => {
+      fakeFile = data;
+      callback(null);
+    });
+    const list = [
+      { title: 'task 1', done: false },
+      { title: 'task 2', done: true }
+    ];
+    await db.write(list, '/testWritePath');
+    expect(fakeFile).toBe(JSON.stringify(list) + '\n');
   });
 });
